4c94111260424b20d29bfb13d2c1f28e9632f14c
Fahima-anny <fahima.info03@gmail.com>
2025-02-12 18:53:42 +0600

    loader updated

--- src/Layouts/Home/Home.jsx
+++ src/Layouts/Home/Home.jsx
@@ -1,59 +1,62 @@
-import Banner from "./Banner";
-import ContactSection from "./ContactSection";
-import FaqSection from "./FaqSection";
-import QueriesSection from "./QueriesSection";
 import { useEffect, useState } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Helmet } from "react-helmet-async";
-import AboutUs from "./AboutUs";
 import { ClipLoader } from "react-spinners";
 
+import Banner from "./Banner";
+import ContactSection from "./ContactSection";
+import FaqSection from "./FaqSection";
+import QueriesSection from "./QueriesSection";
+import AboutUs from "./AboutUs";
 
 const Home = () => {
+  const [loadingData, setLoadingData] = useState(
+    localStorage.getItem("homeLoaded") ? false : true
+  );
 
-  const [loadingData, setLoadingData] = useState(true);
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+      once: true,
+      offset: 100,
+    });
+  }, []);
 
-useEffect(() => {
-  AOS.init({
-    duration: 2000,
-    once: true,
-    offset: 100, 
-  });
-}, []);
+  useEffect(() => {
+    if (!localStorage.getItem("homeLoaded")) {
+      // Simulate loading for 2 seconds
+      const timer = setTimeout(() => {
+        setLoadingData(false);
+        localStorage.setItem("homeLoaded", "true"); // Set flag in localStorage
+      }, 3000);
 
-useEffect(() => {
-  // Simulate loading for 2 seconds
-  const timer = setTimeout(() => {
-    setLoadingData(false);
-  }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, []);
 
-  return () => clearTimeout(timer);
-}, []);
-    
-    return (
-      <div>
-        {loadingData ? (
-          <div className="flex justify-center items-center h-screen bg-white">
-            <ClipLoader color="#3498db" size={50} />
-          </div>
-        ) : (
-          <div className="space-y-5 md:space-y-20">
-          <Helmet>
-                <title>Smart Pick | Home</title>
-            </Helmet>
-           <div  data-aos="fade-in" >
-           <Banner></Banner>
-           </div>
-           <AboutUs></AboutUs>
-            <QueriesSection></QueriesSection>
-           <FaqSection></FaqSection>
-            <ContactSection></ContactSection>
+  return (
+    <div>
+      {loadingData ? (
+        <div className="flex justify-center items-center h-[70vh] bg-white">
+          <ClipLoader color="#3498db" size={50} />
         </div>
-        )}
-      </div>
-    );
-    
+      ) : (
+        <div className="space-y-5 md:space-y-20">
+          <Helmet>
+            <title>Smart Pick | Home</title>
+          </Helmet>
+          <div data-aos="fade-in">
+            <Banner />
+          </div>
+          <AboutUs />
+          <QueriesSection />
+          <FaqSection />
+          <ContactSection />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Home;
